c053d13e9514e823d89820ea60619cb700e76410
ronnieboy2k <ronnie.boy2k@gmail.com>
2026-09-10 20:00:51 +0800

    refactor: remove unused Django signature form

--- signatures/views.py
+++ signatures/views.py
@@ -11,15 +11,6 @@ from .serializers import SignatureSerializer
 
 
 # Create your views here.
-class SignatureFormPageView(TemplateView):
-    template_name = "signatures/signature_form.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-
-        return context
-
-
 class SignatureCreateAPI(CreateAPIView):
     queryset = Signature.objects.all()
     serializer_class = SignatureSerializer
