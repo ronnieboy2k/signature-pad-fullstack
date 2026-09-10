0f6026bd10f63e2b569ec6c00001c38da5fafdc2
ronnieboy2k <ronnie.boy2k@gmail.com>
2026-09-10 19:50:37 +0800

    feat: add signature image column to Signer admin

--- signatures/admin.py
+++ signatures/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Signature, Signer
 
 
 # Register your models here.
 @admin.register(Signer)
 class SignerAdmin(admin.ModelAdmin):
-    list_display = ("name", "signature_path")
+    list_display = ("name", "signature_path", "signature_image")
 
     def signature_path(self, obj):
         signature = obj.signature_set.first()
@@ -15,4 +17,19 @@ class SignerAdmin(admin.ModelAdmin):
 
         return signature.image or "-"
 
-    signature_path.short_description = "Signature"
+    signature_path.short_description = "Signature Path"
+
+    def signature_image(self, obj):
+        signature = obj.signature_set.first()
+
+        if not signature or not signature.image:
+            return "Missing signature image"
+
+        image_url = "/" + signature.image
+
+        return format_html(
+            '<img src="{}" width="150" height="75" ' 'style="object-fit: contain;">',
+            image_url,
+        )
+
+    signature_image.short_description = "Signature Image"
