8d0b2bbe0126efaf071bad9ad4f750531ac3635c
ronnieboy2k <ronnie.boy2k@gmail.com>
2026-09-10 16:51:24 +0800

    test: add Django tests for React frontend export

--- signatures/tests.py
+++ signatures/tests.py
@@ -7,7 +7,7 @@ from .models import Signature, Signer
 # Create your tests here.
 class SignatureFormPageTests(TestCase):
     def setUp(self):
-        self.signature_form_url = reverse("signature_form")
+        self.signature_form_url = reverse("signature")
 
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/signature/")
@@ -17,9 +17,11 @@ class SignatureFormPageTests(TestCase):
         response = self.client.get(self.signature_form_url)
         self.assertEqual(response.status_code, 200)
 
-    def test_template_name_correct(self):
+    def test_react_frontend_is_served(self):
         response = self.client.get(self.signature_form_url)
-        self.assertTemplateUsed(response, "signatures/signature_form.html")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<div id="root"></div>')
 
 
 class SignatureCreateAPITests(TestCase):
